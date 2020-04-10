45145763b963a2d5aea4fa2ba8b9bfc02ca79634
James Wolfe <james.wolfe.2023@student.lhschools.org>
2020-04-10 10:07:02 -0400

    Intellij Commit

--- src/lickingheights/com/Main.java
+++ src/lickingheights/com/Main.java
@@ -1,35 +1,8 @@
 package lickingheights.com;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner keyboard = new Scanner(System.in);
-
-        System.out.println("Please Enter \"Yes\" For Hello or \"No\" For Goodbye! ");
-
-        String answer = keyboard.nextLine();
-
-        if(answer.equalsIgnoreCase("yes")){
-            printHello();
-        }
-        else if(answer.equalsIgnoreCase("no")){
-            printGoodbye();
-        }
-    }
-
-//    else(answer.equalsIgnoreCase != ("yes") / ("no"))
-//
-//    static { System.out.println("You Suck") ;
-//    }
-
-    public static void printHello(){
-        System.out.println("Hello");
-    }
-
-    public static void printGoodbye(){
-        System.out.println("Goodbye");
     }
 }
